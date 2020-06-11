112ce976760965f3c1a48a45cacc9422980a0976
rajeshmaple123 <66706881+rajeshmaple123@users.noreply.github.com>
2020-06-10 22:44:22 -0400

    Add files via upload

--- Repositories/DataAccessRepositories.cs
+++ Repositories/DataAccessRepositories.cs
@@ -7,13 +7,13 @@ using webapiInsurer.Controllers;
 using System.Web.UI;
 using System.Net;
 using System.Net.Http;
-
-
-
-
 using webapiInsurer;
 using static webapiInsurer.Models.ApplicationContext;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices.ComTypes;
+using System.Data.Common;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace webapiInsurer.Repositories
 {
@@ -24,11 +24,13 @@ namespace webapiInsurer.Repositories
         int AddContract(TEntity b);
         int UpdateContract(U custname, TEntity b);
         int DeleteContract(U custname);
+
+        int Addcontractwithproccall(TEntity b);
+
+        int Updatecontractwithproccall(TEntity b,U Custname);
     }
 
-
-
-    public class DataAccessRepository : IDataAccess<Contracts, string>
+        public class DataAccessRepository : IDataAccess<Contracts, string>,IDataAccess<Insert_Contract_Post_Method_API,string>, IDataAccess<Update_Contract_PUT_Method_API, string>
     {
         readonly ApplicationContext ctx;
         public DataAccessRepository(ApplicationContext c)
@@ -40,6 +42,42 @@ namespace webapiInsurer.Repositories
             ctx.Contracts.Add(b);
             int res = ctx.SaveChanges();
             return res;
+        }
+
+        public int AddContract(Insert_Contract_Post_Method_API b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddContract(Update_Contract_PUT_Method_API b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Addcontractwithproccall(Insert_Contract_Post_Method_API b)
+        {
+
+            ctx.Database.ExecuteSqlCommand("EXEC Insert_Contract_Post_Method_API @Name,@Address,@dateofbirth, @gender, @saledate, @country",
+                new SqlParameter("@Name", b.Name),
+                 new SqlParameter("@Address", b.Address),
+                 new SqlParameter("@dateofbirth", b.Dateofbirth),
+                 new SqlParameter("@gender", b.Gender),
+                 new SqlParameter("@saledate", b.Saledate),
+                 new SqlParameter("@country", b.Country));
+     
+            int res = ctx.SaveChanges();
+            ctx.Inscon.Add(b);
+            return res;
+        }
+
+        public int Addcontractwithproccall(Contracts b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Addcontractwithproccall(Update_Contract_PUT_Method_API b)
+        {
+            throw new NotImplementedException();
         }
 
         public int DeleteContract(string custname)
@@ -86,16 +124,87 @@ namespace webapiInsurer.Repositories
                 contracts.CustomerCountry = b.CustomerCountry;
                 contracts.CustomerAddress = b.CustomerAddress;
                 contracts.CustomerGender = b.CustomerGender;
-                contracts.CustomerCountry = b.CustomerCountry;
                 contracts.CustomerDateofbirth = b.CustomerDateofbirth;
                 contracts.SaleDate = b.SaleDate;
-                contracts.NetPrice = b.NetPrice;
-                contracts.CoveragePlan = b.CoveragePlan;
                 res = ctx.SaveChanges();
             }
+
             return res;
         }
 
+        public int UpdateContract(string custname, Insert_Contract_Post_Method_API b)
+        {
+            throw new NotImplementedException();
+        }
 
+        public int UpdateContract(string custname, Update_Contract_PUT_Method_API b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Updatecontractwithproccall(Contracts b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Updatecontractwithproccall(Insert_Contract_Post_Method_API b,Contracts b1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Updatecontractwithproccall(Insert_Contract_Post_Method_API b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Updatecontractwithproccall(Insert_Contract_Post_Method_API b, string Custname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Updatecontractwithproccall(Contracts b, string Custname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Updatecontractwithproccall(Update_Contract_PUT_Method_API b, string Custname)
+        {
+           // var contracts = ctx.Contracts.Find(Custname);
+           
+                _ = ctx.Contracts.Find(Custname);
+                ctx.Database.ExecuteSqlCommand("EXEC Update_Contract_PUT_Method_API @Name,@Address,@dateofbirth, @gender, @saledate, @country",
+                    new SqlParameter("@Name", b.Name),
+                  new SqlParameter("@Address", b.Address),
+                  new SqlParameter("@dateofbirth", b.Dateofbirth),
+                  new SqlParameter("@gender", b.Gender),
+                  new SqlParameter("@saledate", b.Saledate),
+                  new SqlParameter("@country", b.Country));
+            
+
+            int res = ctx.SaveChanges();
+       
+            return res;
+        }
+
+        Insert_Contract_Post_Method_API IDataAccess<Insert_Contract_Post_Method_API, string>.GetContract(string custname)
+        {
+            throw new NotImplementedException();
+        }
+
+        Update_Contract_PUT_Method_API IDataAccess<Update_Contract_PUT_Method_API, string>.GetContract(string custname)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Insert_Contract_Post_Method_API> IDataAccess<Insert_Contract_Post_Method_API, string>.GetContracts()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        IEnumerable<Update_Contract_PUT_Method_API> IDataAccess<Update_Contract_PUT_Method_API, string>.GetContracts()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
